beb020e64f0e62581b9256fc7b9129991c757859
PistaMista <krystofalbrecht@icloud.com>
2017-07-07 15:50:18 +0200

    Fix.

---
+++ Assets/Scripts/User Interface/Interface Elements/Battle UI/AttackView_UIElement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackView_UIElement : UIElement
+{
+    public override void Enable ()
+    {
+        base.Enable();
+        UserInterface.managedBattle.selectedPlayer.board.visualModules[2].Enable();
+    }
+
+    public override void Disable ()
+    {
+        base.Disable();
+        UserInterface.managedBattle.selectedPlayer.board.visualModules[2].Disable();
+    }
+
+    public override void OnBattleChange ()
+    {
+        base.OnBattleChange();
+        if (UserInterface.managedBattle.selectedPlayer != null && !gameObject.activeInHierarchy)
+        {
+            Enable();
+        }
+        else if (gameObject.activeInHierarchy)
+        {
+            Disable();
+        }
+    }
+}
---
+++ Assets/Scripts/User Interface/Interface Elements/Board Visual Modules/AttackView_BoardVisualModule.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackView_BoardVisualModule : BoardVisualModule
+{
+    public override void Initialize ()
+    {
+        base.Initialize();
+    }
+
+    public override void Enable ()
+    {
+        base.Enable();
+        board.visualModules[1].Enable();
+        board.ReinitializeGrid();
+
+        foreach (Ship ship in board.owner.ships)
+        {
+            ship.gameObject.SetActive( !ship.eliminated && ship.revealedBy.Contains( UserInterface.managedBattle.activePlayer ) );
+        }
+
+        float boardSide = Mathf.Sqrt( board.tiles.Length );
+        float elevation = 0.5f + ( boardSide / 2f ) / Mathf.Atan( Camera.main.fieldOfView / 2f * Mathf.Deg2Rad );
+        Cameraman.AddWaypoint( board.owner.transform.position + Vector3.up * elevation, Vector3.down, 0.3f, Mathf.Infinity, false );
+    }
+
+    public override void Disable ()
+    {
+        base.Disable();
+        board.visualModules[1].Disable();
+    }
+
+    public override void Refresh ()
+    {
+        base.Refresh();
+    }
+}
